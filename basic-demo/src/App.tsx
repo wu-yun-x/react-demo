b739a1901dafe077e21a668ca78e818ccdd49fe0
st004362 <chuangchuang.mi@santachi.com.cn>
2025-10-09 17:53:41 +0800

    feat: 内容添加

--- basic-demo/src/App.tsx
+++ basic-demo/src/App.tsx
@@ -2,16 +2,17 @@
  * @Author: st004362
  * @Date: 2025-04-03 11:13:05
  * @LastEditors: ST/St004362
- * @LastEditTime: 2025-09-24 14:51:28
+ * @LastEditTime: 2025-09-29 15:04:18
  * @Description: APP
  */
 import "./App.css";
-import UseInsertionEffectDemo from "./views/useInsertionEffect";
+// import UseInsertionEffectDemo from "./views/useInsertionEffect";
+import APP from "./views/useLayoutEffect";
 
 function App() {
   return (
     <>
-      <UseInsertionEffectDemo />
+      <APP />
     </>
   );
 }
